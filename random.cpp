aa79c0ac7971d6a2e0f1fc7d49ef3c767468d5ea
Dan Kurtzer <dsk19360@gmail.com>
2015-03-07 10:41:21 -0500

    added update-git.sh

--- random.cpp
+++ random.cpp
@@ -7,10 +7,10 @@ int main(void){
 
 	srand( time( NULL) ); // seed random number from time
 	int random_number = 1 + (rand() % 10); // random number between 1 and 10
-	// declare variable to store guess of user
+	// declare variable to store guess of user and set to 0
 	// declare variable as counter for number of guesses
 
-
+	
 	for(;;)
 	{
 		// increment guess counter

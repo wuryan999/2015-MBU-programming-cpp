4272e1020379a1cd9abb00eae26157721681a10b
Ryan Wu <wuryan999@gmail.com>
2015-04-04 14:37:27 +0000

    Automatic commit

---
+++ starter.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+
+int main(void){
+    int i=3;
+    //cout << i << endl;
+    cin >> i;
+    if (i==3)
+    {
+        cout << "hello world" << endl;
+    }
+    else if (4 == i)
+    {
+        cout << "good afternoon" << endl;
+    }
+    else
+    {
+        cout << "good night" << endl;
+    }
+
+
+
+	return 0;
+}
